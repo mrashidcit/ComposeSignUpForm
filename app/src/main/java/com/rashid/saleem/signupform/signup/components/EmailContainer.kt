d8005e11eb44d428be75e1491d927309ab5a013d
Rashid Saleem <mrashidcit@gmail.com>
2025-07-29 09:00:04 +0500

    feat: Complete the FullName & Email Container UI

---
+++ app/src/main/java/com/rashid/saleem/signupform/signup/components/EmailContainer.kt
@@ -0,0 +1,58 @@
+package com.rashid.saleem.signupform.signup.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rashid.saleem.signupform.ui.theme.SignUpFormTheme
+
+@Composable
+fun EmailContainer(modifier: Modifier = Modifier) {
+    Column {
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("Email")
+            }
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("Verification Code")
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EmailContainerPreview() {
+    SignUpFormTheme {
+        Surface {
+            EmailContainer()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
