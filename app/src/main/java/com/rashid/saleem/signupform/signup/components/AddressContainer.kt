4a74388cb130789f33b0ba94b524c5f8e0b4719e
Rashid Saleem <mrashidcit@gmail.com>
2025-08-07 08:54:10 +0500

    feat: Complete the Address Container UI

---
+++ app/src/main/java/com/rashid/saleem/signupform/signup/components/AddressContainer.kt
@@ -0,0 +1,84 @@
+package com.rashid.saleem.signupform.signup.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rashid.saleem.signupform.ui.theme.SignUpFormTheme
+
+@Composable
+fun AddressContainer() {
+
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("Country")
+            }
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = {
+                Text("City")
+            }
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        TextField(
+            value = "",
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+            ,
+            placeholder = {
+                Text("Address")
+            }
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+
+    }
+}
+
+
+@Preview
+@Composable
+private fun AddressContainerPreview() {
+    SignUpFormTheme {
+        Surface {
+            AddressContainer()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
